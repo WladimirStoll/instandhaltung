a98ed640e640450ca8b325ce64d5bffa4131f75d
WladimirStoll <wladimir.stoll@gmx.de>
2017-04-16 09:33:16 +0200

    v20170416_instandhaltung_neuer_rechner

--- bin/cert_create1.bat
+++ bin/cert_create1.bat
@@ -1,4 +1,4 @@
 echo on
-keytool.exe -v -genkey -alias MyLager -keystore ../dist/lib/lager.keystore -keypass lager2lager2lager2 -storepass lager3 -dname CN=Stoll
-keytool -list -keystore ./../dist/lib/lager.keystore -storepass lager3 
+keytool.exe -v -genkey -alias MyLager -keystore ../dist/lib/lager.keystore -keypass lager2lager2lager2 -storepass lager3 -dname CN=Stoll -validity 3600
+keytool -list -keystore ./../dist/lib/lager.keystore -storepass lager3 -validity 3600
   
--- src/cert_create2.bat
+++ src/cert_create2.bat
@@ -1,4 +1,4 @@
 echo on
-keytool.exe -v -genkey -alias MyLager -keystore ../dist/lib/lager.keystore -keypass lager2lager2lager2 -storepass lager3 -dname CN=Stoll
+keytool.exe -v -genkey -alias MyLager -keystore ../dist/lib/lager.keystore -keypass lager2lager2lager2 -storepass lager3 -dname CN=Stoll -validity 3600
 keytool -selfcert -alias MyLager -keystore ./../dist/lib/lager.keystore -storepass lager3 -keypass lager2lager2lager2
 keytool -list -keystore ./../dist/lib/lager.keystore -storepass lager3 
